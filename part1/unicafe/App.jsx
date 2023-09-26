ededd67c3751b62535458622fc2111b261b78908
DVerSan <davidverdeguersantolaria@gmail.com>
2023-09-26 10:51:12 +0200

    Exercise 1.6 - unicafe step1

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 
+const Button = (props) => (
+  <div>
+    <button onClick={props.handleClick}>{props.text}</button>
+  </div>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const Button = (props) => (
-    <div>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </div>
-  )
 
   return (
     <>
