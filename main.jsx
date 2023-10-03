678e0c2e3d4ba21b26478ebe74fe41c87a1d011c
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-03 15:16:34 +0200

    main.jsx moved to project root

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from './part1/unicafe/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
