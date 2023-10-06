1578fb0bfdea710aa5f783212799abd43e8a39ef
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-06 11:24:46 +0200

    ESLint rule added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    indent: 'off'
+    indent: 'off',
+    'multiline-ternary': 'off'
   }
 }
