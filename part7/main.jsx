345f0b5a34708362d8299bbb2eed078164148124
DVerSan <davidverdeguersantolaria@gmail.com>
2023-11-29 18:37:05 +0100

    Exercise 7.8 - Ultimate hooks

--- part7/main.jsx
+++ part7/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './countryHook/App.jsx'
+import App from './notesHook/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
