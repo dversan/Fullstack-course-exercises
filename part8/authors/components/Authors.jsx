cd48219802d437c98448682ca1db9fa41a4de34f
DVerSan <davidverdeguersantolaria@gmail.com>
2023-12-19 18:30:23 +0100

    Exercise 8.11 - Authors birth year

--- part8/authors/components/Authors.jsx
+++ part8/authors/components/Authors.jsx
@@ -1,6 +1,24 @@
-import { Table } from 'react-bootstrap'
+import { Button, Form, Table } from 'react-bootstrap'
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { EDIT_AUTHOR } from '../queries.js'
 
 const Authors = ({ authors }) => {
+  const [authName, setAuthName] = useState('')
+  const [birthDate, setBirthDate] = useState('')
+  const [editAuthor] = useMutation(EDIT_AUTHOR)
+
+  const submitHandler = async (event) => {
+    event.preventDefault()
+
+    await editAuthor({
+      variables: { name: authName, born: birthDate }
+    })
+
+    setAuthName('')
+    setBirthDate('')
+  }
+
   return (
     <div className={'container'}>
       <h2>authors</h2>
@@ -20,6 +38,32 @@ const Authors = ({ authors }) => {
           ))}
         </tbody>
       </Table>
+      <h3>Set birthyear</h3>
+      <Form onSubmit={submitHandler}>
+        <div>
+          name
+          <input
+            type={'text'}
+            name={'authName'}
+            value={authName}
+            onChange={(e) => setAuthName(e.target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type={'number'}
+            name={'born'}
+            value={birthDate}
+            onChange={(e) => setBirthDate(Number(e.target.value))}
+          />
+        </div>
+        <div className={'pt-2'}>
+          <Button className={'btn btn-sm'} type={'submit'}>
+            Update Author
+          </Button>
+        </div>
+      </Form>
     </div>
   )
 }
