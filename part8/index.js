27cee1f4e4e18a045692ac2dff73f4633f00f88f
DVerSan <davidverdeguersantolaria@gmail.com>
2024-01-24 18:09:20 +0100

    Exercise 8.23 - Subscriptions server

--- part8/index.js
+++ part8/index.js
@@ -281,27 +281,6 @@ const resolvers = {
   }
 }
 
-// startStandaloneServer does not allow adding subscriptions to the application.
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers
-// })
-
-// startStandaloneServer(server, {
-//   listen: { port: 4000 },
-//   context: async ({ req, res }) => {
-//     const auth = req ? req.headers.authorization : null
-//     if (auth && auth.startsWith('Bearer ')) {
-//       const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-//       const currentUser = await User.findById(decodedToken.id).populate('books')
-//       return { currentUser }
-//     }
-//   }
-// }).then(({ url }) => {
-//   console.log(`Server ready at ${url}`)
-// })
-
 // using Express middleware, which means that Express must also be configured for the application, with the GraphQL server acting as middleware
 
 const start = async () => {
